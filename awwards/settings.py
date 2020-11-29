0f80a772fbfba5882534f67a8b47de3d0e470be9
felkiriinya <felkiriinya@gmail.com>
2020-11-29 13:51:57 +0300

    deployment to heroku

--- awwards/settings.py
+++ awwards/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
