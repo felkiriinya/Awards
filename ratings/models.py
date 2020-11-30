6464c3ca4fcd4233412835d0819d220f76bea69e
felkiriinya <felkiriinya@gmail.com>
2020-11-30 16:54:02 +0300

    altered a model field

--- ratings/models.py
+++ ratings/models.py
@@ -64,7 +64,7 @@ class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     technologies = models.CharField(max_length=100)
     categories = models.CharField(max_length=100)
-    desc = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
     def __str__(self):
         return self.sitename
 
